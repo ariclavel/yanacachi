01b2a0e1c699fe5c35224113f95c8c5dbc43341e
ariclavel <a01706629@itesm.mx>
2022-03-10 12:12:28 -0400

    home

---
+++ yanacachi/src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Rooms</h1>
+                    <span className='subtitle'>Book now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+ 
+export default HomePage;
